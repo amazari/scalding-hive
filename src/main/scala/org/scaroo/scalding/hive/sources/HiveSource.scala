d78d98029556ce0568984773d733435910e816a9
Alexandre Mazari <a.mazari@criteo.com>
2015-03-03 18:59:14 +0100

    Add a Scalding source based on cascading-hive HiveViewAnalyzer

--- src/main/scala/org/scaroo/scalding/hive/sources/HiveSource.scala
+++ src/main/scala/org/scaroo/scalding/hive/sources/HiveSource.scala
@@ -1,8 +1,20 @@
 package org.scaroo.scalding.hive.sources
 
-/**
- * Created by a.mazari on 03/03/2015.
- */
-class HiveSource {
+import cascading.tap.{MultiSourceTap, Tap}
+import cascading.tap.hive.{HiveViewAnalyzer, }
+import com.twitter.scalding._
 
+
+case class HiveSource(query: String)(implicit analyzer: HiveViewAnalyzer) extends Source {
+  override def createTap(readOrWrite: AccessMode)(implicit mode: Mode): Tap[_, _, _] =
+    mode match  {
+      case Local(_) | Hdfs(_, _) => {
+        new MultiSourceTap(analyzer.asTaps(query))
+      }
+      case _ => ???
+  }
+}
+
+object HiveSource {
+  implicit lazy val analyzer = new HiveViewAnalyzer()
 }
